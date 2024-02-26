0ac837c767f8e5186cc43e0720ca3467c20b751e
matlavv <mathis.laversin92@gmail.com>
2024-02-26 09:57:42 +0100

    Date into header creation

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
 
 const Header = () => {
+  const today = new Date();
+  const dateString = today.toLocaleDateString("fr-FR", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  });
+
   return (
     <SafeAreaView>
-      <View>
-        <Text>Header</Text>
+      <View style={tw`flex`}>
+        <Text style={tw`text-[#999999] m-3 text-base`}>{dateString}</Text>
       </View>
     </SafeAreaView>
   );
